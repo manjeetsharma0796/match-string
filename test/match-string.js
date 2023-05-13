bc40f31dd1dae37df8208f8c91c9ba607dc80ed3
Manjeet Sharma <manjeetsharma0796@users.noreply.github.com>
2023-05-13 14:26:50 +0530

    Add match, which matched two text and it is functional

--- test/match-string.js
+++ test/match-string.js
@@ -1,7 +1,7 @@
 const {describe, it} = require('node:test');
-const {strictEqual}  = require('assert');
+const {strictEqual, deepStrictEqual}  = require('assert');
 
-const {extractLetter, extractDigitsFromStart} = require('../src/match-string.js');
+const {extractLetter, extractDigitsFromStart, match} = require('../src/match-string.js');
 
 describe('extractLetter', () => {
   it('should extract letter from a text', () => {
@@ -12,5 +12,17 @@ describe('extractLetter', () => {
 describe('extractDigitsFromStart', () => {
   it('should give consecutive digits from begin of text', () => {
     strictEqual(extractDigitsFromStart('121helo'), '121');
-  })
-})
+  });
+});
+
+describe('match', () => {
+  it('should match two texts if prefix letters matches with suffix of other text', () => {
+    const text1 = ['123til'];
+    const text2 = ['til902'];
+
+    const actual = match(text1, text2);
+    const expected = ['123til902'];
+
+    deepStrictEqual(actual, expected);
+  });
+});
