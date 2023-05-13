9abd066672e4cece807eee168a189780fc19c932
Manjeet Sharma <manjeetsharma0796@users.noreply.github.com>
2023-05-13 13:03:45 +0530

    Add TODO and read, which reads files content

--- src/match-string.js
+++ src/match-string.js
@@ -1,5 +1,12 @@
+const fs  = require('fs')
+
+const read = function(filePath) {
+  return fs.readFileSync(filePath, 'utf-8');
+};
+
 const rename = function() {
   return true;
 };
 
+console.log(read('./resource/file2'));
 exports.rename = rename;
